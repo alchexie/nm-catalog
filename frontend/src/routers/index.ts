100940907d66e440e060e0b8e24c8157ffc3e8be
wanghuai <wanghuai@pwrd.com>
2025-11-20 18:16:06 +0800

    feat: add playlist feature with grouping and routing

--- frontend/src/routers/index.ts
+++ frontend/src/routers/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeComponent from '@/pages/Home';
 import DetailComponent from '@/pages/Detail';
+import PlaylistComponent from '@/pages/Playlist';
 // import UploadComponent from '@/pages/Upload.vue';
 
 const routes = [
   { path: '/', component: HomeComponent },
   { path: '/:gid', component: DetailComponent },
+  { path: '/playlist/:pid', component: PlaylistComponent },
   // { path: '/upload', component: UploadComponent },
 ];
 
